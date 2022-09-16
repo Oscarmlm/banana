6370b7be5c444d0f13a5bf1291188d9c696633a6
Renato De la Rosa <srdelarosab@gmail.com>
2022-09-15 19:50:55 -0500

    Poke API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bananaApp = () => {
   const alertMessage = async (msg, title, author) => {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
     const data = await response.json();
-    console.log(data);
+    console.log(`📢 POKE API: ${data}`);
     console.log(`📢 ${author}: ${title}/${msg}`);
   };
   return { alertMessage };
