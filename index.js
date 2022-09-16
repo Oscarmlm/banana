bf51e348676584c8a4b1edbacc4411089955c134
Renato De la Rosa <srdelarosab@gmail.com>
2022-09-15 19:45:28 -0500

    Ajustes Urgentes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const bananaApp = () => {
-  const alertMessage = (msg, title, author) => {
+  const alertMessage = async (msg, title, author) => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    const data = await response.json();
+    console.log(data);
     console.log(`📢 ${author}: ${title}/${msg}`);
   };
   return { alertMessage };
